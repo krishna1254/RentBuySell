51f45b8c94dc9c51302eb3e30d04a6a2a9cf44a6
krishna1254 <32281733+krishna1254@users.noreply.github.com>
2025-03-20 20:12:59 +0530

    Add project files.

---
+++ RentBuySell/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RentBuySell.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
